2103aa32093a926eeb52b94f0e59575005f9f693
SripaliManimekala <sripalimanimekala@gmail.com>
2023-10-31 21:37:52 +0530

    created two buttons and handle click event

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello moda siripali',e);
+    }
+
+    const handleClickAgain = (name, e) =>{
+        console.log('hi '+name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h1>this is home page</h1>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={(e)=>handleClickAgain('chamod',e)}>click me again</button>
         </div>
      );
 }
