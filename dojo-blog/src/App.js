0648c7e502b6552eca9b57272aace2df54e20a6f
SripaliManimekala <sripalimanimekala@gmail.com>
2023-10-31 19:45:38 +0530

    created root component and navbar component and homepage component

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+
+  const title = 'welcome to the new blog';
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="content">
+        <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
