7b3335013204ba0bb5304ac3e924e800bd51a663
SripaliManimekala <sripalimanimekala@gmail.com>
2023-11-01 17:50:21 +0530

    use bloglist component with diffrent data with the use of props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -7,20 +7,17 @@ const Home = () => {
 
     const [blogs,setBlogs] = useState([
         // each object represent a single blogs.
-        {id:1 , title:"First Blog", body:"This is the first blog",author:'zoro'},
+        {id:1 , title:"First Blog", body:"This is the first blog",author:'momonuske'},
         {id:2 , title:"Second Blog", body:"This is the second blog",author:'shanks'},
         {id:3 , title:"Third Blog", body:"This is the third blog",author:'momonuske'}
     ]);
 
-    const handleCLick =()=>{
-        setLikes(likes+1);
-    }
-
     return ( 
         <div className="home">
-            <h1>total likes { likes } </h1>
-            <button onClick={handleCLick}>like us</button>
+            {/* taking all blogs in */}
             <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter((blog)=>blog.author==='momonuske')} title="Momonuske's Blogs"/>
+            <BlogList blogs={blogs.filter((blog)=>blog.author==='shanks')} title="Shanks's Blogs"/>
         </div>
      );
 }
