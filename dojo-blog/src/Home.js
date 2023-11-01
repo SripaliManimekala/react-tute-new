c63f255c0f3e54043cff9807ba025afbae2e9cbd
SripaliManimekala <sripalimanimekala@gmail.com>
2023-11-01 13:13:24 +0530

    created a like buttun with increment likes using useState hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,21 +1,24 @@
 import { useState } from 'react'; 
 
 const Home = () => {
-    //let name='mario';
-    const [name,setName] = useState('mario');
-    const handleClick = () => {
-        setName('luffy');
+
+    const[likes,setLikes] = useState(0);
+
+    const handleCLick =()=>{
+        setLikes(likes+1);
     }
 
-    const handleClickAgain = (name, e) =>{
-        console.log('hi '+name, e.target);
-    }
+    const [blogs,setBlogs] = useState([
+        // each object represent a single blogs.
+        {id:1 , title:"First Blog", body:"This is the first blog",author:'zoro'},
+        {id:2 , title:"Second Blog", body:"This is the second blog",author:'shanks'},
+        {id:3 , title:"Third Blog", body:"This is the third blog",author:'momonuske'}
+    ]);
 
     return ( 
         <div className="home">
-            <h1>this is home page</h1>
-            <p>{name}</p>
-            <button onClick={handleClick}>click me</button>
+            <h1>total likes { likes } </h1>
+            <button onClick={handleCLick}>like us</button>
         </div>
      );
 }
