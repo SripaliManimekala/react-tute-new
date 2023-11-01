a9057f67d784a10826596d1268b7937f5c357fb0
SripaliManimekala <sripalimanimekala@gmail.com>
2023-11-01 20:55:09 +0530

    learn to use useEffect hook which runs at every render of a component

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,7 @@
-import { useState } from 'react'; 
+import { useState,useEffect } from 'react'; 
 import BlogList from './BlogList';
 
 const Home = () => {
-
-    const[likes,setLikes] = useState(0);
 
     const [blogs,setBlogs] = useState([
         // each object represent a single blogs.
@@ -12,18 +10,25 @@ const Home = () => {
         {id:3 , title:"Third Blog", body:"This is the third blog",author:'momonuske'}
     ]);
 
+    const [name,setName] = useState('Chopper');
+
     const handleDelete=(id)=>{
         // return a new filtered array without deteled item 
         const newBlogs = blogs.filter((blog)=>blog.id!==id);
         setBlogs(newBlogs);
     }
+    //this hook runs every time there is a re-render
+    useEffect(()=>{
+        console.log('use effect hook ran');
+        console.log(blogs);
+    });
 
     return ( 
         <div className="home">
             {/* taking all blogs in */}
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author==='momonuske')} title="Momonuske's Blogs"/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author==='shanks')} title="Shanks's Blogs"/>
+            <button onClick={()=>setName('Jimbei')}>Change name</button>
+            <p>{ name }</p>
         </div>
      );
 }
