69140cfbe9039762cac1a1c8030bf828e62d2e2e
SripaliManimekala <sripalimanimekala@gmail.com>
2023-11-06 00:36:14 +0530

    add conditional template and shows msg loading to load blogs..in this case it happens fast since we use a local server..to show this a timeout is used

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,33 +4,27 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs,setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
 
-    // const [name,setName] = useState('Chopper');
-
-    const handleDelete=(id)=>{
-        // return a new filtered array without deteled item 
-        const newBlogs = blogs.filter((blog)=>blog.id!==id);
-        setBlogs(newBlogs);
-    }
     //this hook runs every time there is a re-render
     useEffect(()=>{
-        fetch('http://localhost:8000/blogs')//get request to this end point
-        .then(res=>{//this return a promise(async code)
-           return res.json();
-        })
-        .then(data=>{//after above res.json is coplete this will happen,also async
-            // console.log(data);//console the response data
-            setBlogs(data);
-        })
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')//get request to this end point
+            .then(res=>{//this return a promise(async code)
+               return res.json();
+            })
+            .then(data=>{//after above res.json is coplete this will happen,also async
+                // console.log(data);//console the response data
+                setBlogs(data);
+                setIsPending(false);
+            })
+        },1000)
     },[]);
 
     return ( 
         <div className="home">
-            {/* taking all blogs in */}
-            {/* li dynamic check bcz 1st null blogs are loaed it takes some time to load the blogs */}
-            {/* this is some js..so we put curly braces */}
-            {/* check 1st if left is true,if flase dont even bother with write and 1st blogs is null and null evaluates to false */}
-            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+            {isPending&& <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
