9b59d2b6c23810dd44a5c315938e9e2d65df35c9
SripaliManimekala <sripalimanimekala@gmail.com>
2023-11-06 01:43:42 +0530

    handling errors with fetch data from local JSON server

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -5,25 +5,35 @@ const Home = () => {
 
     const [blogs,setBlogs] = useState(null);
     const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
 
     //this hook runs every time there is a re-render
     useEffect(()=>{
         setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')//get request to this end point
-            .then(res=>{//this return a promise(async code)
+            fetch('http://localhost:8000/blogs')
+            .then(res=>{
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource');
+                }
                return res.json();
             })
-            .then(data=>{//after above res.json is coplete this will happen,also async
-                // console.log(data);//console the response data
+            .then(data=>{
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
+            })
+            .catch(err=>{
+                setIsPending(false);
+                setError(err.message);
             })
         },1000)
     },[]);
 
     return ( 
         <div className="home">
-            {isPending&& <div>Loading...</div>}
+            {/* conditionally render based on state */}
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
