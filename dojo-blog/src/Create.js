c5b8f3046282b21ccd1e65f1c9934e87dad40984
SripaliManimekala <sripalimanimekala@gmail.com>
2023-11-21 02:00:03 +0530

    create a temppalte to create blogs in create blog page

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input 
+                   type="text" 
+                   required
+                />
+                <label>Blog Body:</label>
+                <textarea 
+                   required
+                ></textarea>
+                <label>Blog Author:</label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
