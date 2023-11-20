8dfa255308dfc80f64e4fb97d53253956b7c0cb3
SripaliManimekala <sripalimanimekala@gmail.com>
2023-11-20 10:36:58 +0530

    use USEFETCH to get blog data error and pending state inside blog details component

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
+
 const BlogDetails = () => {
+    const { id } = useParams();
+    const { data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/'+ id)
     return ( 
         <div className="blog-details">
-            <h2>Blog Details</h2>
+            <h2>Blog Details - { id }</h2>
         </div>
      );
 }
